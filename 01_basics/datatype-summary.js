7f365ccfb073ac1ca1ec7b5a18b97af1f3745b62
SaugatBhattaraii <bhattaraisaugat39@gmail.com>
2024-06-01 15:17:31 +0545

    datatype-summary

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -12,9 +12,9 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
-// const bigNumber = 3456543576654356754n
+const bigNumber = 3456543576654356754n
 
 
 
@@ -32,6 +32,13 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+console.log(typeof myObj);
+/*
+type of myobj = object
+type of myfunction = function (it is called as object function)
+type of bignumber = bigint
+*/
+
+
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
